a5bf303a9ab545c89931b250025330349661ac07
Carlos-Pach <cpach003@ucr.edu>
2019-11-27 15:46:07 -0800

    new header and cpp

---
+++ src/gimbalnode/src/PID_Func.cpp
@@ -0,0 +1,41 @@
+/*
+ * PID_gimbal.cpp
+ * 
+ * Copyright 2019 CarlosPacheco <carlospacheco@carlospacheco-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <cmath>
+#include "PID_gimbal.h"				//header from local directory
+
+using namespace std ;
+
+void initPID(controlVars *ptr){
+	
+}
+
+void setGains(ptr, kp, ki, kd){
+	
+}
+
+double calculateOutput(controlVars *ptr, error){
+	error =  
+}
